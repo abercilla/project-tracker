223b021ef45e470018d4dcb0874c40337073a822
Anne Bercilla <annebercilla@gmail.com>
2021-05-25 16:45:53 +0000

    Completed Lab and dumped hackbright into hackbright.sql
    Co-Authored by: Annie Ausen <ausen.annie@gmail.com>

--- hackbright.sql
+++ hackbright.sql
@@ -21,6 +21,42 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: grades; Type: TABLE; Schema: public; Owner: hackbright
+--
+
+CREATE TABLE public.grades (
+    id integer NOT NULL,
+    github character varying(30),
+    project_title character varying(30),
+    grade integer
+);
+
+
+ALTER TABLE public.grades OWNER TO hackbright;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE; Schema: public; Owner: hackbright
+--
+
+CREATE SEQUENCE public.grades_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.grades_id_seq OWNER TO hackbright;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: hackbright
+--
+
+ALTER SEQUENCE public.grades_id_seq OWNED BY public.grades.id;
+
+
+--
 -- Name: projects; Type: TABLE; Schema: public; Owner: hackbright
 --
 
@@ -93,6 +129,13 @@ ALTER SEQUENCE public.students_id_seq OWNED BY public.students.id;
 
 
 --
+-- Name: grades id; Type: DEFAULT; Schema: public; Owner: hackbright
+--
+
+ALTER TABLE ONLY public.grades ALTER COLUMN id SET DEFAULT nextval('public.grades_id_seq'::regclass);
+
+
+--
 -- Name: projects id; Type: DEFAULT; Schema: public; Owner: hackbright
 --
 
@@ -107,6 +150,20 @@ ALTER TABLE ONLY public.students ALTER COLUMN id SET DEFAULT nextval('public.stu
 
 
 --
+-- Data for Name: grades; Type: TABLE DATA; Schema: public; Owner: hackbright
+--
+
+COPY public.grades (id, github, project_title, grade) FROM stdin;
+1	jhacks	Markov	10
+2	jhacks	Blockly	2
+3	sdeveloper	Markov	50
+4	sdeveloper	Blockly	100
+5	abercilla	New App	40
+6	aausen	Turtle	25
+\.
+
+
+--
 -- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: hackbright
 --
 
@@ -116,6 +173,7 @@ COPY public.projects (id, title, description, max_grade) FROM stdin;
 3	Annie	Food expy	100
 4	Turtle	Drawing Project	50
 5	Anne project	Social justice events	100
+6	New App	Social network	43
 \.
 
 
@@ -130,10 +188,17 @@ COPY public.students (id, first_name, last_name, github) FROM stdin;
 
 
 --
+-- Name: grades_id_seq; Type: SEQUENCE SET; Schema: public; Owner: hackbright
+--
+
+SELECT pg_catalog.setval('public.grades_id_seq', 6, true);
+
+
+--
 -- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: hackbright
 --
 
-SELECT pg_catalog.setval('public.projects_id_seq', 5, true);
+SELECT pg_catalog.setval('public.projects_id_seq', 6, true);
 
 
 --
@@ -141,6 +206,14 @@ SELECT pg_catalog.setval('public.projects_id_seq', 5, true);
 --
 
 SELECT pg_catalog.setval('public.students_id_seq', 2, true);
+
+
+--
+-- Name: grades grades_pkey; Type: CONSTRAINT; Schema: public; Owner: hackbright
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_pkey PRIMARY KEY (id);
 
 
 --
